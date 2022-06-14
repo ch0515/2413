26b5bd2529daad516899d879113210559aa68b1e
chaemin <w2114@e-mirim.hs.kr>
2022-06-14 14:59:16 +0900

    js 여러가지 함수 모습 복습

---
+++ 20220607_food court/js/main.js
@@ -0,0 +1,8 @@
+// function say(){
+//     console.log('hello world1');
+// }
+// const say = function(){
+//     console.log('hello world2');
+// }
+const say = () => console.log('hello world3');
+say();
