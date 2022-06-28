540777bc8ffb794b27ffc78ff8b1696f67329ef7
chaemin <w2114@e-mirim.hs.kr>
2022-06-28 14:24:27 +0900

    .

--- 20220607_food court/js/main.js
+++ 20220607_food court/js/main.js
@@ -24,3 +24,35 @@ toggleMenu('nav-toggle', 'nav-list');
 // }
 // const say = () => console.log('hello world3');
 // say();
+const addNow = (homeCardId) =>{
+    //html -> js
+    const homeCard = document.getElementById(homeCardId);
+
+    //시간 -> index번째 식사가 선택되어야하는지 결정
+    //조식 끝 : 8 : 00
+    //중식 끝 : 13 : 00
+    //석식 끝 : 17 : 50
+    var now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    console.log(hour , minute);
+    // index = hour%12;
+    hour = 18;
+    minute = 0;
+    if(minute >= 50){
+        hour = hour + 1;
+    }else{
+        hour = hour - 1;
+    }
+    if(hour >= 8 && hour <= 13){ //중식
+        index=1;
+    }else if(hour > 13 && hour <= 18){ //석식
+        index=2;
+    }else{
+        index=0;
+    }
+    //homeCard에서 index번째 card에 new 클래스 추가
+    let card =  homeCard.getElementsByClassName('card')[index];
+    card.classList.add('now');
+}
+addNow('home-card');
